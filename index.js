a603e5e382a118155484c4038481d3484bfae039
Paul D Handy <paul.d.handy@gmail.com>
2017-03-21 09:44:55 -0600

    added catch for null balances

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function generateAddresses() {
 
   for (i = 0; i < config.seeds.length; i++) {
     iotajs.api.getNewAddress(config.seeds[i], {'index': 0, 'total': config.nAddr}, function(_, addresses) {
-      
+
       getBalance(addresses); //Calls the getBalance function with all generated addresses as argument 
-      
+
     });
   };
 };
@@ -23,19 +23,23 @@ function generateAddresses() {
 //Takes a list of addresses as argument and should get the balance for each address
 function getBalance(addresses) {
   iotajs.api.getBalances(addresses, 100, function(error, inputs) {
-	var i = 0; 
-	var totalValue = 0; 
-    inputs.balances.forEach(function(balance) {
-		totalValue += parseInt(balance); 
-		if (parseInt(balance) > 0) {
-			console.log(i+1 + " The address " + addresses[i] + " has a balance of: " + parseInt(balance));
-			console.log("Balance detected!!!");
-		} else {
-			console.log(i+1 + " The address " + addresses[i] + " has a balance of: " + parseInt(balance));
-		};
-	i++
-    })
-	console.log("All addresses of this seeds contain " + totalValue + " tokens!")
+    var i = 0; 
+    var totalValue = 0; 
+    if(inputs.balances != null) {
+      inputs.balances.forEach(function(balance) {
+        totalValue += parseInt(balance); 
+        if (parseInt(balance) > 0) {
+          console.log(i+1 + " The address " + addresses[i] + " has a balance of: " + parseInt(balance));
+          console.log("Balance detected!!!");
+        } else {
+          console.log(i+1 + " The address " + addresses[i] + " has a balance of: " + parseInt(balance));
+        };
+        i++
+      })
+    } else {
+      console.log(error);
+    }
+    console.log("All addresses of this seeds contain " + totalValue + " tokens!")
   });
 };
 generateAddresses();
